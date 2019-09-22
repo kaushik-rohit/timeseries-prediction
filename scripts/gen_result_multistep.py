86a1d16e09b31355b90b9c03ab5f817671c7d802
Rohit Kaushik <kaushikrohit325@gmail.com>
2019-09-22 19:04:20 +0200

    add plot_all function to plot network
    Add an implementation for plotting network png
    to be used in the research paper.

--- scripts/gen_result_multistep.py
+++ scripts/gen_result_multistep.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import sys
 from copy import deepcopy
 
 from sklearn.preprocessing import MinMaxScaler
@@ -18,6 +19,7 @@ from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras.models import load_model
 from keras.layers import LSTM, GRU, Conv1D, MaxPooling1D
+from keras.utils.vis_utils import plot_model
 from operator import add
 import json
 #INFY','BHARTIARTL', 'ULTRACEMCO', 'CIPLA', 'ACC', 
@@ -105,152 +107,170 @@ def test_model(model, X_init, Y_test, window_size):
 	return error, Y_test_pred
 		
 	
+def plot_all_model():
+	print('plotting_model')
+	ann_model = make_ann_model(3)
+	cnn_model = make_cnn_model(3)
+	lstm_model = make_lstm_model(3)
+	gru_model = make_gru_model(3)
+	
+	plot_model(ann_model, to_file='ann_plot.png', show_shapes=True, show_layer_names=True)
+	plot_model(cnn_model, to_file='cnn_plot.png', show_shapes=True, show_layer_names=True)
+	plot_model(lstm_model, to_file='lstm_plot.png', show_shapes=True, show_layer_names=True)
+	plot_model(gru_model, to_file='gru_plot.png', show_shapes=True, show_layer_names=True)
+	
 	
 if __name__ == '__main__':
+	if len(sys.argv) > 2:
+		print('invalid number of arguments')
+	elif len(sys.argv) == 2:
+		if sys.argv[1] == 'plot':
+			print('plotting')
+			plot_all_model()
+	else:
+		result = []
 
-	result = []
+		for sym in symbols:
 
-	for sym in symbols:
+			json_descriptor = {'stock':sym,'ann':[], 'gru':[], 'lstm':[], 'cnn':[]}
 
-		json_descriptor = {'stock':sym,'ann':[], 'gru':[], 'lstm':[], 'cnn':[]}
+			try:
+				data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
+			except:
+				print('----------------Sleeping for 5 minutes. Too much Load-------------------------')
+				time.sleep(300)
+				data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
+			data.index = pd.to_datetime(data.index)
+			data_frame = data.copy()
+			df = data_frame[["Close"]]
 
-		try:
-			data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
-		except:
-			print('----------------Sleeping for 5 minutes. Too much Load-------------------------')
-			time.sleep(300)
-			data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
-		data.index = pd.to_datetime(data.index)
-		data_frame = data.copy()
-		df = data_frame[["Close"]]
+			split_date = pd.Timestamp('01-01-2017')
 
-		split_date = pd.Timestamp('01-01-2017')
+			train = df.loc[:split_date]
+			test = df.loc[split_date:]
 
-		train = df.loc[:split_date]
-		test = df.loc[split_date:]
+			sc = MinMaxScaler()
+			train_sc = sc.fit_transform(train)
+			test_sc = sc.transform(test)
 
-		sc = MinMaxScaler()
-		train_sc = sc.fit_transform(train)
-		test_sc = sc.transform(test)
-
-		ann = []
-		gru = []
-		lstm = []
-		cnn = []
-		
-
-		for win_sz in window_size:
-			ann_result = []
-			gru_result = []
-			lstm_result = []
-			cnn_result = []
-			pred_ANN = []
-			pred_LSTM = []
-			pred_GRU = []
-			pred_CNN = []
-			for i in range(5):
-				#''''''''ANN'''''''''''''''''''
-				X_train, y_train = window_transform(train_sc, win_sz)
-
-				model = make_ann_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=2, verbose=1)
-				history = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, callbacks=[early_stop], shuffle=False)
-				
-				error, pred_ann = test_model(model, X_train[-1], test_sc, win_sz)
-				
-				ann_result.append(error)
-				#''''''''ANN''''''''''''''''''''
-				
-				
-				#''''''''CNN''''''''''''''''''''
-				model = make_cnn_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
-				history_model_cnn = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
-				
-				error, pred_cnn = test_model(model, X_train[-1], test_sc, win_sz)
-				cnn_result.append(error)
-				#''''''''CNN''''''''''''''''''''
-				
-				
-				X_tr_t = X_train.reshape(X_train.shape[0], win_sz, 1)
-				
-				#''''''''''LSTM''''''''''''''''''
-				model = make_lstm_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
-				history_model_lstm = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
-				
-				error, pred_lstm = test_model(model, X_train[-1], test_sc, win_sz)
-				lstm_result.append(error)
-				#"''''''''LSTM'''''''''''''''''''
-				
-				#''''''''GRU'''''''''''''''''''''
-				model = make_gru_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
-				history_model_gru = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
-				
-				error, pred_gru = test_model(model, X_train[-1], test_sc, win_sz)
-				gru_result.append(error)
-				#''''''''GRU'''''''''''''''''''''
-				
-				pred_ANN.append(pred_ann)
-				pred_CNN.append(pred_cnn)
-				pred_LSTM.append(pred_lstm)
-				pred_GRU.append(pred_gru)
-			#update optimal window size param
-			ann.append([win_sz, min(ann_result), np.mean(ann_result), np.std(ann_result)])
-			gru.append([win_sz, min(gru_result), np.mean(gru_result), np.std(gru_result)])
-			lstm.append([win_sz, min(lstm_result), np.mean(lstm_result), np.std(lstm_result)])
-			cnn.append([win_sz, min(cnn_result), np.mean(cnn_result), np.std(cnn_result)])
+			ann = []
+			gru = []
+			lstm = []
+			cnn = []
 			
-			plot_ann = [0]*len(pred_ANN[0])
-			for pred in pred_ANN:
-				plot_ann = list(map(add, plot_ann, pred))
+
+			for win_sz in window_size:
+				ann_result = []
+				gru_result = []
+				lstm_result = []
+				cnn_result = []
+				pred_ANN = []
+				pred_LSTM = []
+				pred_GRU = []
+				pred_CNN = []
+				for i in range(5):
+					#''''''''ANN'''''''''''''''''''
+					X_train, y_train = window_transform(train_sc, win_sz)
+
+					model = make_ann_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=2, verbose=1)
+					history = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, callbacks=[early_stop], shuffle=False)
+					
+					error, pred_ann = test_model(model, X_train[-1], test_sc, win_sz)
+					
+					ann_result.append(error)
+					#''''''''ANN''''''''''''''''''''
+					
+					
+					#''''''''CNN''''''''''''''''''''
+					model = make_cnn_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
+					history_model_cnn = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
+					
+					error, pred_cnn = test_model(model, X_train[-1], test_sc, win_sz)
+					cnn_result.append(error)
+					#''''''''CNN''''''''''''''''''''
+					
+					
+					X_tr_t = X_train.reshape(X_train.shape[0], win_sz, 1)
+					
+					#''''''''''LSTM''''''''''''''''''
+					model = make_lstm_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
+					history_model_lstm = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
+					
+					error, pred_lstm = test_model(model, X_train[-1], test_sc, win_sz)
+					lstm_result.append(error)
+					#"''''''''LSTM'''''''''''''''''''
+					
+					#''''''''GRU'''''''''''''''''''''
+					model = make_gru_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
+					history_model_gru = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
+					
+					error, pred_gru = test_model(model, X_train[-1], test_sc, win_sz)
+					gru_result.append(error)
+					#''''''''GRU'''''''''''''''''''''
+					
+					pred_ANN.append(pred_ann)
+					pred_CNN.append(pred_cnn)
+					pred_LSTM.append(pred_lstm)
+					pred_GRU.append(pred_gru)
+				#update optimal window size param
+				ann.append([win_sz, min(ann_result), np.mean(ann_result), np.std(ann_result)])
+				gru.append([win_sz, min(gru_result), np.mean(gru_result), np.std(gru_result)])
+				lstm.append([win_sz, min(lstm_result), np.mean(lstm_result), np.std(lstm_result)])
+				cnn.append([win_sz, min(cnn_result), np.mean(cnn_result), np.std(cnn_result)])
 				
-			for i in range(len(plot_ann)):
-				plot_ann[i] = plot_ann[i]/5
+				plot_ann = [0]*len(pred_ANN[0])
+				for pred in pred_ANN:
+					plot_ann = list(map(add, plot_ann, pred))
+					
+				for i in range(len(plot_ann)):
+					plot_ann[i] = plot_ann[i]/5
+				
+				plot_lstm = [0]*len(pred_LSTM[0])
+				for pred in pred_LSTM:
+					plot_lstm = list(map(add, plot_lstm, pred))
+					
+				for i in range(len(plot_lstm)):
+					plot_lstm[i] = plot_lstm[i]/5
+					
+				plot_gru = [0]*len(pred_GRU[0])
+				for pred in pred_GRU:
+					plot_gru = list(map(add, plot_gru, pred))
+					
+				for i in range(len(plot_gru)):
+					plot_gru[i] = plot_gru[i]/5
+					
+				plot_cnn = [0]*len(pred_CNN[0])
+				for pred in pred_CNN:
+					plot_cnn = list(map(add, plot_cnn, pred))
+					
+				for i in range(len(plot_cnn)):
+					plot_cnn[i] = plot_cnn[i]/5
+					
+					
+				#save prediction plots
+				plt.plot(test_sc, label='True Values', color='black')
+				plt.plot(plot_ann, label='ANN Prediction', color='red')
+				plt.plot(plot_cnn, label='CNN Prediction', color='blue')
+				plt.plot(plot_lstm, label='LSTM Prediction', color='green')
+				plt.plot(plot_gru, label='GRU Prediction', color='yellow')
+				plt.title("Prediction")
+				plt.xlabel('Time')
+				plt.ylabel('Normalized Stock Prices')
+				plt.legend()
+				plt.savefig('./plots/' + sym + ' ' + ' window_sz ' + str(win_sz))
+				plt.clf()
+				
+				
+			json_descriptor['ann'] = ann
+			json_descriptor['lstm'] = lstm
+			json_descriptor['cnn'] = cnn
+			json_descriptor['gru'] = gru 
+			result.append(json_descriptor)
 			
-			plot_lstm = [0]*len(pred_LSTM[0])
-			for pred in pred_LSTM:
-				plot_lstm = list(map(add, plot_lstm, pred))
-				
-			for i in range(len(plot_lstm)):
-				plot_lstm[i] = plot_lstm[i]/5
-				
-			plot_gru = [0]*len(pred_GRU[0])
-			for pred in pred_GRU:
-				plot_gru = list(map(add, plot_gru, pred))
-				
-			for i in range(len(plot_gru)):
-				plot_gru[i] = plot_gru[i]/5
-				
-			plot_cnn = [0]*len(pred_CNN[0])
-			for pred in pred_CNN:
-				plot_cnn = list(map(add, plot_cnn, pred))
-				
-			for i in range(len(plot_cnn)):
-				plot_cnn[i] = plot_cnn[i]/5
-				
-				
-			#save prediction plots
-			plt.plot(test_sc, label='True Values', color='black')
-			plt.plot(plot_ann, label='ANN Prediction', color='red')
-			plt.plot(plot_cnn, label='CNN Prediction', color='blue')
-			plt.plot(plot_lstm, label='LSTM Prediction', color='green')
-			plt.plot(plot_gru, label='GRU Prediction', color='yellow')
-			plt.title("Prediction")
-			plt.xlabel('Time')
-			plt.ylabel('Normalized Stock Prices')
-			plt.legend()
-			plt.savefig('./plots/' + sym + ' ' + ' window_sz ' + str(win_sz))
-			plt.clf()
-			
-			
-		json_descriptor['ann'] = ann
-		json_descriptor['lstm'] = lstm
-		json_descriptor['cnn'] = cnn
-		json_descriptor['gru'] = gru 
-		result.append(json_descriptor)
-		
-		with open('data.json', 'w') as f:
-			json.dump(result, f)
-		#json.dumps(result)
+			with open('data.json', 'w') as f:
+				json.dump(result, f)
+			#json.dumps(result)
--- scripts/single-step.py
+++ scripts/single-step.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import sys
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import r2_score
@@ -17,6 +18,7 @@ from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras.models import load_model
 from keras.layers import LSTM, GRU, Conv1D, MaxPooling1D
+from keras.utils.vis_utils import plot_model
 from operator import add
 import json
 #'ULTRACEMCO', 'CIPLA', 'ACC', 'HDFC',, 'HCLTECH', 'JSWSTEEL', 'MARUTI'
@@ -87,158 +89,178 @@ def test_model(model, X_train, X_test, y_train, y_test):
 	r2_test = mse(y_test, y_pred_test_ann)
 	
 	return r2_train, r2_test
+
+def plot_all_model():
+	print('plotting_model')
+	ann_model = make_ann_model(3)
+	cnn_model = make_cnn_model(3)
+	lstm_model = make_lstm_model(3)
+	gru_model = make_gru_model(3)
 	
+	plot_model(ann_model, to_file='ann_plot.png', show_shapes=True, show_layer_names=True)
+	plot_model(cnn_model, to_file='cnn_plot.png', show_shapes=True, show_layer_names=True)
+	plot_model(lstm_model, to_file='lstm_plot.png', show_shapes=True, show_layer_names=True)
+	plot_model(gru_model, to_file='gru_plot.png', show_shapes=True, show_layer_names=True)
+	
+
 if __name__ == '__main__':
+	
+	if len(sys.argv) > 2:
+		print('invalid number of arguments')
+	elif len(sys.argv) == 2:
+		if sys.argv[1] == 'plot':
+			print('plotting')
+			plot_all_model()
+	else:
+		result = []
 
-	result = []
+		for sym in symbols:
 
-	for sym in symbols:
+			json_descriptor = {'stock':sym,'ann':[], 'gru':[], 'lstm':[], 'cnn':[]}
 
-		json_descriptor = {'stock':sym,'ann':[], 'gru':[], 'lstm':[], 'cnn':[]}
+			try:
+				data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
+			except:
+				print('----------------Sleeping for 5 minutes. Too much Load-------------------------')
+				time.sleep(300)
+				data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
+			data.index = pd.to_datetime(data.index)
+			data_frame = data.copy()
+			df = data_frame[["Close"]]
 
-		try:
-			data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
-		except:
-			print('----------------Sleeping for 5 minutes. Too much Load-------------------------')
-			time.sleep(300)
-			data = get_history(symbol=sym, start=date(2002,1,1), end=date(2019,1,15))
-		data.index = pd.to_datetime(data.index)
-		data_frame = data.copy()
-		df = data_frame[["Close"]]
+			split_date = pd.Timestamp('01-01-2017')
 
-		split_date = pd.Timestamp('01-01-2017')
+			train = df.loc[:split_date]
+			test = df.loc[split_date:]
 
-		train = df.loc[:split_date]
-		test = df.loc[split_date:]
+			sc = MinMaxScaler()
+			train_sc = sc.fit_transform(train)
+			test_sc = sc.transform(test)
 
-		sc = MinMaxScaler()
-		train_sc = sc.fit_transform(train)
-		test_sc = sc.transform(test)
-
-		ann = []
-		gru = []
-		lstm = []
-		cnn = []
-		
-
-		for win_sz in window_size:
-			ann_result = []
-			gru_result = []
-			lstm_result = []
-			cnn_result = []
-			pred_ANN = []
-			pred_LSTM = []
-			pred_GRU = []
-			pred_CNN = []
-			for i in range(5):
-				#''''''''ANN'''''''''''''''''''
-				X_train, y_train = window_transform(train_sc, win_sz)
-				X_test, y_test = window_transform(test_sc, win_sz)
-
-				model = make_ann_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=2, verbose=1)
-				history = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, callbacks=[early_stop], shuffle=False)
-				
-				train_acc, test_acc = test_model(model, X_train, X_test, y_train, y_test)
-				y_pred_test_ANN = model.predict(X_test)
-				
-				ann_result.append(test_acc)
-				#''''''''ANN''''''''''''''''''''
-				
-				
-				#''''''''CNN''''''''''''''''''''
-				model = make_cnn_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
-				history_model_cnn = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
-				
-				train_acc, test_acc = test_model(model, X_train, X_test, y_train, y_test)
-				y_pred_test_CNN = model.predict(X_test)
-				cnn_result.append(test_acc)
-				#''''''''CNN''''''''''''''''''''
-				
-				
-				X_tr_t = X_train.reshape(X_train.shape[0], win_sz, 1)
-				X_tst_t = X_test.reshape(X_test.shape[0], win_sz, 1)
-				
-				#''''''''''LSTM''''''''''''''''''
-				model = make_lstm_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
-				history_model_lstm = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
-				
-				train_acc, test_acc = test_model(model, X_tr_t, X_tst_t, y_train, y_test)
-				y_pred_test_LSTM = model.predict(X_tst_t)
-				lstm_result.append(test_acc)
-				#"''''''''LSTM'''''''''''''''''''
-				
-				#''''''''GRU'''''''''''''''''''''
-				model = make_gru_model(win_sz)
-				early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
-				history_model_gru = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
-				
-				train_acc, test_acc = test_model(model, X_tr_t, X_tst_t, y_train, y_test)
-				y_pred_test_GRU = model.predict(X_tst_t)
-				gru_result.append(test_acc)
-				#''''''''GRU'''''''''''''''''''''
-				
-				pred_ANN.append(y_pred_test_ANN)
-				pred_CNN.append(y_pred_test_CNN)
-				pred_LSTM.append(y_pred_test_LSTM)
-				pred_GRU.append(y_pred_test_GRU)
-			#update optimal window size param
-			ann.append([win_sz, min(ann_result), np.mean(ann_result), np.std(ann_result)])
-			gru.append([win_sz, min(gru_result), np.mean(gru_result), np.std(gru_result)])
-			lstm.append([win_sz, min(lstm_result), np.mean(lstm_result), np.std(lstm_result)])
-			cnn.append([win_sz, min(cnn_result), np.mean(cnn_result), np.std(cnn_result)])
+			ann = []
+			gru = []
+			lstm = []
+			cnn = []
 			
-			plot_ann = [0]*len(pred_ANN[0])
-			for pred in pred_ANN:
-				plot_ann = list(map(add, plot_ann, pred))
+
+			for win_sz in window_size:
+				ann_result = []
+				gru_result = []
+				lstm_result = []
+				cnn_result = []
+				pred_ANN = []
+				pred_LSTM = []
+				pred_GRU = []
+				pred_CNN = []
+				for i in range(5):
+					#''''''''ANN'''''''''''''''''''
+					X_train, y_train = window_transform(train_sc, win_sz)
+					X_test, y_test = window_transform(test_sc, win_sz)
+
+					model = make_ann_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=2, verbose=1)
+					history = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, callbacks=[early_stop], shuffle=False)
+					
+					train_acc, test_acc = test_model(model, X_train, X_test, y_train, y_test)
+					y_pred_test_ANN = model.predict(X_test)
+					
+					ann_result.append(test_acc)
+					#''''''''ANN''''''''''''''''''''
+					
+					
+					#''''''''CNN''''''''''''''''''''
+					model = make_cnn_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
+					history_model_cnn = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
+					
+					train_acc, test_acc = test_model(model, X_train, X_test, y_train, y_test)
+					y_pred_test_CNN = model.predict(X_test)
+					cnn_result.append(test_acc)
+					#''''''''CNN''''''''''''''''''''
+					
+					
+					X_tr_t = X_train.reshape(X_train.shape[0], win_sz, 1)
+					X_tst_t = X_test.reshape(X_test.shape[0], win_sz, 1)
+					
+					#''''''''''LSTM''''''''''''''''''
+					model = make_lstm_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
+					history_model_lstm = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
+					
+					train_acc, test_acc = test_model(model, X_tr_t, X_tst_t, y_train, y_test)
+					y_pred_test_LSTM = model.predict(X_tst_t)
+					lstm_result.append(test_acc)
+					#"''''''''LSTM'''''''''''''''''''
+					
+					#''''''''GRU'''''''''''''''''''''
+					model = make_gru_model(win_sz)
+					early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
+					history_model_gru = model.fit(X_tr_t, y_train, epochs=200, batch_size=32, verbose=1, shuffle=False, callbacks=[early_stop])
+					
+					train_acc, test_acc = test_model(model, X_tr_t, X_tst_t, y_train, y_test)
+					y_pred_test_GRU = model.predict(X_tst_t)
+					gru_result.append(test_acc)
+					#''''''''GRU'''''''''''''''''''''
+					
+					pred_ANN.append(y_pred_test_ANN)
+					pred_CNN.append(y_pred_test_CNN)
+					pred_LSTM.append(y_pred_test_LSTM)
+					pred_GRU.append(y_pred_test_GRU)
+				#update optimal window size param
+				ann.append([win_sz, min(ann_result), np.mean(ann_result), np.std(ann_result)])
+				gru.append([win_sz, min(gru_result), np.mean(gru_result), np.std(gru_result)])
+				lstm.append([win_sz, min(lstm_result), np.mean(lstm_result), np.std(lstm_result)])
+				cnn.append([win_sz, min(cnn_result), np.mean(cnn_result), np.std(cnn_result)])
 				
-			for i in range(len(plot_ann)):
-				plot_ann[i] = plot_ann[i]/5
+				plot_ann = [0]*len(pred_ANN[0])
+				for pred in pred_ANN:
+					plot_ann = list(map(add, plot_ann, pred))
+					
+				for i in range(len(plot_ann)):
+					plot_ann[i] = plot_ann[i]/5
+				
+				plot_lstm = [0]*len(pred_LSTM[0])
+				for pred in pred_LSTM:
+					plot_lstm = list(map(add, plot_lstm, pred))
+					
+				for i in range(len(plot_lstm)):
+					plot_lstm[i] = plot_lstm[i]/5
+					
+				plot_gru = [0]*len(pred_GRU[0])
+				for pred in pred_GRU:
+					plot_gru = list(map(add, plot_gru, pred))
+					
+				for i in range(len(plot_gru)):
+					plot_gru[i] = plot_gru[i]/5
+					
+				plot_cnn = [0]*len(pred_CNN[0])
+				for pred in pred_CNN:
+					plot_cnn = list(map(add, plot_cnn, pred))
+					
+				for i in range(len(plot_cnn)):
+					plot_cnn[i] = plot_cnn[i]/5
+					
+					
+				#save prediction plots
+				plt.plot(y_test, label='True Values', color='black')
+				plt.plot(plot_ann, label='ANN Prediction', color='red')
+				plt.plot(plot_cnn, label='CNN Prediction', color='blue')
+				plt.plot(plot_lstm, label='LSTM Prediction', color='green')
+				plt.plot(plot_gru, label='GRU Prediction', color='yellow')
+				plt.title("Prediction")
+				plt.xlabel('Time')
+				plt.ylabel('Normalized Stock Prices')
+				plt.legend()
+				plt.savefig('./plots/' + sym + ' ' + ' window_sz ' + str(win_sz))
+				plt.clf()
+				
+				
+			json_descriptor['ann'] = ann
+			json_descriptor['lstm'] = lstm
+			json_descriptor['cnn'] = cnn
+			json_descriptor['gru'] = gru 
+			result.append(json_descriptor)
 			
-			plot_lstm = [0]*len(pred_LSTM[0])
-			for pred in pred_LSTM:
-				plot_lstm = list(map(add, plot_lstm, pred))
-				
-			for i in range(len(plot_lstm)):
-				plot_lstm[i] = plot_lstm[i]/5
-				
-			plot_gru = [0]*len(pred_GRU[0])
-			for pred in pred_GRU:
-				plot_gru = list(map(add, plot_gru, pred))
-				
-			for i in range(len(plot_gru)):
-				plot_gru[i] = plot_gru[i]/5
-				
-			plot_cnn = [0]*len(pred_CNN[0])
-			for pred in pred_CNN:
-				plot_cnn = list(map(add, plot_cnn, pred))
-				
-			for i in range(len(plot_cnn)):
-				plot_cnn[i] = plot_cnn[i]/5
-				
-				
-			#save prediction plots
-			plt.plot(y_test, label='True Values', color='black')
-			plt.plot(plot_ann, label='ANN Prediction', color='red')
-			plt.plot(plot_cnn, label='CNN Prediction', color='blue')
-			plt.plot(plot_lstm, label='LSTM Prediction', color='green')
-			plt.plot(plot_gru, label='GRU Prediction', color='yellow')
-			plt.title("Prediction")
-			plt.xlabel('Time')
-			plt.ylabel('Normalized Stock Prices')
-			plt.legend()
-			plt.savefig('./plots/' + sym + ' ' + ' window_sz ' + str(win_sz))
-			plt.clf()
-			
-			
-		json_descriptor['ann'] = ann
-		json_descriptor['lstm'] = lstm
-		json_descriptor['cnn'] = cnn
-		json_descriptor['gru'] = gru 
-		result.append(json_descriptor)
-		
-		with open('data.json', 'w') as f:
-			json.dump(result, f)
-		#json.dumps(result)
+			with open('data.json', 'w') as f:
+				json.dump(result, f)
+			#json.dumps(result)
