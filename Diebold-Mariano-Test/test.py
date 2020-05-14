65efe01643d222cdf758f6d7b60239ff7352b8f8
Rohit Kaushik <kaushikrohit325@gmail.com>
2020-05-14 23:11:34 +0200

    update testing strategy

--- Diebold-Mariano-Test/test.py
+++ Diebold-Mariano-Test/test.py
@@ -14,14 +14,15 @@ def test():
         lstm = df['lstm'].tolist()
         gru = df['gru'].tolist()
         
-        param1 = dm_test.dm_test(actual, ann, cnn)
-        param2 = dm_test.dm_test(actual, lstm, gru)
-        param3 = dm_test.dm_test(actual, ann, lstm)
+        param1 = dm_test.dm_test(actual, ann, cnn, crit='poly', power=10)
+        param2 = dm_test.dm_test(actual, lstm, gru, crit='poly', power=10)
+        param3 = dm_test.dm_test(actual, ann, lstm, crit='poly', power=10)
+        param4 = dm_test.dm_test(actual, lstm, cnn, crit='poly', power=10)
         
-        row += [param1, param2, param3]
+        row += [param1, param2, param3, param4]
         rows.append(row)
         
-    res = pd.DataFrame(rows, columns=['stock', 'ann-cnn', 'lstm-gru', 'ann-lstm'])
+    res = pd.DataFrame(rows, columns=['stock', 'ann-cnn', 'lstm-gru', 'ann-lstm', 'lstm-cnn'])
     res.to_csv(path_or_buf='./hypothesis_test_results.csv')
 if __name__ == '__main__':
     test()
